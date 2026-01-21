bd0375de0827a4b3f2369b23623eb9c4f8deb297
Jeeya04 <161134495+Jeeya04@users.noreply.github.com>
2026-01-21 15:56:07 +0530

    Add conditional pg2

---
+++ python_conditions/pg2.py
@@ -0,0 +1,10 @@
+# pg2: Determine result based on student marks
+
+marks = int(input("Enter marks: "))
+
+if marks >= 80:
+    print("Distinction")
+elif marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
